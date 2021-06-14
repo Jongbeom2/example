06d98d05dc15543236be70d9a7df26e06e260156
사용자 <apple@sayongjaui-MacBookPro.local>
2021-06-14 18:15:10 +0900

    ios kakao 설정

---
+++ client_app/ios/SwiftBridge.swift
@@ -0,0 +1,8 @@
+//
+//  SwiftBridge.swift
+//  client_app
+//
+//  Created by 사용자 on 2021/06/14.
+//
+
+import Foundation
--- client_app/ios/client_app/AppDelegate.m
+++ client_app/ios/client_app/AppDelegate.m
@@ -4,6 +4,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <RNKakaoLogins.h>
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -57,6 +59,16 @@ static void InitializeFlipper(UIApplication *application) {
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)app
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
+
+ return NO;
 }
 
 @end
