0b2e7af0c70e79f64e5c964874e8e9b0024e03b6
사용자 <apple@sayongjaui-MacBookPro.local>
2021-06-14 20:41:36 +0900

    naver map 세팅

--- client_app/src/App.js
+++ client_app/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {ApolloProvider} from '@apollo/client';
-import client from 'src/apollo/client';
-import Main from './Main';
+// import {ApolloProvider} from '@apollo/client';
+// import client from 'src/apollo/client';
+import {Text} from 'react-native';
+// import Main from './Main';
 const App = ({...rest}) => {
-  return (
-    <ApolloProvider client={client}>
-      <Main {...rest} />
-    </ApolloProvider>
-  );
+  // return (
+  //   <ApolloProvider client={client}>
+  //     <Main {...rest} />
+  //   </ApolloProvider>
+  // );
+  return (<Text>Hi</Text>)
 };
 
 export default App;
