4805c2486965a41709067158e3debd05e148eb8f
사용자 <n11334@naver.com>
2021-07-23 18:27:54 +0900

    getnow from server

--- client/src/page/cloudfront/CloudfrontMain.jsx
+++ client/src/page/cloudfront/CloudfrontMain.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useQuery } from '@apollo/client';
+import { GET_NOW } from './cloudfront.query';
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(5),
@@ -8,12 +10,17 @@ const useStyles = makeStyles((theme) => ({
 }));
 const CloudfrontMain = () => {
   const classes = useStyles();
-  const date =
+  const { data, loading, error } = useQuery(GET_NOW);
+  const clientDate =
     new Date().toLocaleDateString() + new Date().toLocaleTimeString();
+  const serverDate =
+    new Date(data?.getNow).toLocaleDateString() +
+    new Date(data?.getNow).toLocaleTimeString();
   return (
     <div className={classes.root}>
       <div>Cloudfront Test</div>
-      <div>Date from Client: {date}</div>
+      <div>Date from Client: {clientDate}</div>
+      <div>Date from Server: {serverDate}</div>
     </div>
   );
 };
---
+++ client/src/page/cloudfront/cloudfront.query.js
@@ -0,0 +1,7 @@
+import { gql } from '@apollo/client';
+
+export const GET_NOW = gql`
+  query getNow {
+    getNow
+  }
+`;
